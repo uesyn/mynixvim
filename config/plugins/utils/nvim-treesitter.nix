de345b56bc11e39fc88b58d1466286941c3d52fb
uesyn <suemura@zlab.co.jp>
2024-05-22 14:14:12 +0900

    Use pre-build plugins in nvim-treesitter

--- config/plugins/utils/nvim-treesitter.nix
+++ config/plugins/utils/nvim-treesitter.nix
@@ -1,20 +1,16 @@
 {pkgs, ...}: {
-  extraPackages = with pkgs; [
-    tree-sitter
-  ];
-
   extraPlugins = with pkgs.vimPlugins; [
-    nvim-treesitter
+    (nvim-treesitter.withPlugins (
+      plugins: with plugins; [
+        markdown
+        markdown_inline
+      ]
+    ))
   ];
 
   extraConfigLua = ''
-    local parser_dir = vim.fn.expand('~/.cache/treesitter/parser')
-    vim.opt.runtimepath:append(parser_dir)
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = { "markdown", "markdown_inline" },
-      sync_install = true,
       auto_install = false,
-      parser_install_dir = parser_dir,
     }
   '';
 }
